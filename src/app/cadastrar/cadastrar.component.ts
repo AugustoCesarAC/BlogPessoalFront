25cf6f68a9c0395bc9b17f2027db05f30963c6f0
Augusto Cesar <acsilvaluis@outlook.com>
2022-07-04 13:27:11 -0300

    Consumo da API com Cadastro e Login

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { UsuarioLogin } from '../model/UsuarioLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
   templateUrl: './cadastrar.component.html',
-  styleUrls: ['./cadastrar.component.css']
+  styleUrls: ['./cadastrar.component.css'],
 })
 export class CadastrarComponent implements OnInit {
+  usuario: Usuario = new Usuario();
+  confirmSenha: string;
+  tipoUsuario: string;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    window.scroll(0, 0);
   }
 
+  confirmarSenha(event: any) {
+    this.confirmSenha = event.target.value;
+  }
+
+  tipoUser(event: any) {
+    this.tipoUsuario = event.target.value;
+  }
+
+  cadastrar() {
+    this.usuario.tipo = this.tipoUsuario;
+
+    if (this.usuario.senha != this.confirmSenha) {
+      alert('Senhas diferentes!');
+    } else {
+      this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) => {
+        this.usuario = resp;
+        this.router.navigate(['/entrar']);
+        alert('Usuário cadastrado com sucesso!');
+      });
+    }
+  }
 }
